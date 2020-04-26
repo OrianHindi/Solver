8b6de9a2507b405d81fe841d7c8a714e353ed91b
yiratpeleg <yiratpeleg8@gmail.com>
2020-04-26 10:53:09 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/matala3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matala3.dir/main.cpp.o"
+  "matala3"
+  "matala3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matala3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/matala3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for matala3.
+# This may be replaced when dependencies are built.
